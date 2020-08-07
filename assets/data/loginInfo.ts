af07f7e4f5f233bc9bbf2ecdfb24ff38fc1d26d1
Sukhbir Singh <44106929+sukhbir77@users.noreply.github.com>
2020-08-07 16:41:28 -0700

    Add files via upload

---
+++ assets/data/loginInfo.ts
@@ -0,0 +1,10 @@
+import { sing9014 } from '../../app/sing9014';
+
+export const MYCLASSES: sing9014 [] = [
+  {
+    student_name: 'Sukhbir Singh',
+    student_number: '991545757',
+    campus: 'Davis',
+    login_name: 'sing9014',
+    assignment_title: 'Assignment 4',
+  }];
